167e6c6a55526bc35116055f600db2def6353313
iceliades <theau.descatoire@ensea.fr>
2020-10-24 01:12:41 +0200

    ajout de state.cpp pour les test unitaires

---
+++ src/shared/state/state.cpp
@@ -0,0 +1,35 @@
+//
+// Created by theau on 24/10/2020.
+//
+#include <string>
+#include "Element.h"
+#include <boost/test/unit_test.hpp>
+
+namespace state{
+
+    void Element::setTileCode(int x){
+        this->tileCode = x;
+    };
+
+    int Element::getTileCode(){
+        return this->tileCode;
+    };
+
+    std::string Element::getName(){
+        return this->name;
+    }
+
+    BOOST_AUTO_TEST_CASE(TestStaticAssert){
+        BOOST_CHECK(1);
+    };
+
+    BOOST_AUTO_TEST_CASE(TestState){
+        Element e;
+        BOOST_CHECK_EQUAL(e.getTileCode,1);
+        int x = 6;
+        e.setTileCode(x);
+        BOOST_CHECK_EQUAL(e.getTileCode(),x);
+        BOOST_CHECK_EQUAL(e.getName(),);
+    };
+
+};
