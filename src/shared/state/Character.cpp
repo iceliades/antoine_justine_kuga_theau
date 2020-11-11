eb5ae61488d07e25a502666cd027e2ee32cc5e66
Kuga23 <ku.ga@hotmail.fr>
2020-11-11 21:07:34 +0100

    state

---
+++ src/shared/state/Character.cpp
@@ -0,0 +1,128 @@
+#include "Character.h"
+#include "State.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+using namespace state;
+
+
+Character::Character(CharacterTypeID id, std::string name, int x, int y, int tileCode){
+
+    this->tileCode=tileCode;
+    TypeID=id;
+    Name= name;
+    Status= AVAILABLE;
+    //charWeap= new Weapon(SWORD);
+    Movement = 5;
+
+    if (id==CROOK){
+        Health=100;
+        Precision=2; 
+        Dodge = 0;
+       // stats.setAgility(stats.getAgility()+1);// Taxon bonus
+       
+    }else if (id==KNIGHT)
+    {
+        Health=80;
+        Precision=3; 
+        Dodge = 0;
+      //  stats.setAgility(stats.getAgility()+1);
+        
+    }else if (id==ELF){
+
+    }else if (id==NATIVE){
+
+    }else if (id==DWARF){
+
+    }else if (id==PIRATE)
+    {
+        /* code */
+    }else if (id==TROLL)
+    {
+        /* code */
+    }
+    
+}
+
+Character::~Character(){
+   // delete charWeap;
+}
+
+// Getters
+CharacterTypeID Character::getTypeID() {return TypeID;}
+std::string   Character::getName (){return Name;}
+CharacterStatusID Character::getStatus (){return Status;};
+int Character::getMovement (){return Movement;}
+int Character::getHealth (){return Health;}
+float Character::getDodge (){return Dodge;}
+float Character::getPrecision (){return Precision;}
+
+/*Effect& Character::getEffect (){
+    Effect &refEff = effect;
+    return refEff;
+}
+Stats& Character::getStats (){
+    Stats &refStats = stats;
+    return refStats;
+}
+Weapon* Character::getCharWeap (){
+    return charWeap;
+}
+*/
+
+
+// Setters
+void Character::setHealth ( int Stamina,  int Strength){
+    
+    Health= 3*Stamina + 2*Strength;
+}
+
+void Character::setMovementBonus (int Courage, int Stamina){
+    Movement= int(5+ Courage/12 + Stamina/12);
+}
+
+void Character::setDodge (int Agility, int Intelligence){
+    Dodge= (Agility+Intelligence)/60;
+}
+
+void Character::setPrecision ( int Agility, int Intelligence, int Strength, int Arcane){
+    Precision= (Agility+Intelligence+Strength+Arcane)/60;
+}
+/*void Character::setEffect (bool Immobilised,bool Stunned,bool Disarmed){
+    effect.setImmobilised(Immobilised);
+    effect.setStunned(Stunned);
+    effect.setDisarmed(Disarmed);
+};
+
+void Character::setCharWeap (Weapon* w){
+    charWeap=w;
+}
+
+void Character::setStats (int Agility, int Arcane, int Courage, int Intelligence, int Stamina, int Strength){
+    stats.setAgility(Agility);
+    stats.setArcane(Arcane);
+    stats.setCourage(Courage);
+    stats.setIntelligence(Intelligence);
+    stats.setStamina(Stamina);
+    stats.setStrength(Strength);
+}
+*/
+
+void Character::setTypeID(CharacterTypeID TypeID){
+    // TypeID can't change
+}
+void Character::setName(const std::string& Name){
+    this->Name=Name;
+}
+void Character::setStatus(CharacterStatusID Status){
+    this->Status=Status;
+}
+void Character::setMovement(int Movement){
+    this->Movement=Movement;
+}
+
+
--- test/shared/test_shared_state.cpp
+++ test/shared/test_shared_state.cpp
@@ -1,10 +1,11 @@
 //
 // Created by theau on 24/10/2020.
-//
-    #include <boost/test/unit_test.hpp>
+// 
+#include <boost/test/unit_test.hpp>
 #include <string>
 #include "../../src/shared/state.h"
-#include "../../src/shared/state/Element.h"
+//#include "../../src/shared/state/Element.h"
+//#include "../../src/shared/state/Character.h"
 
 
 namespace state{
@@ -16,12 +17,78 @@ namespace state{
 
         BOOST_AUTO_TEST_CASE(TestState)
         {
-            Element e;
-            int x =1;
+            {
+                Element e;
+                int x =1;
 
-            e.setTileCode(x);
-            e.setName("Name");
-            BOOST_CHECK_EQUAL(e.getTileCode(), 1);
-            BOOST_CHECK_EQUAL(e.getName(), "Name");
-        };
-};
+                e.setTileCode(x);
+                e.setName("Name");
+                BOOST_CHECK_EQUAL(e.getTileCode(), 1);
+                BOOST_CHECK_EQUAL(e.getName(), "Name");
+            }
+
+            {
+                // Character
+                Character Crook(CROOK, "crook", 10, 10, 1);
+                //BOOST_CHECK_EQUAL(Crook.getPosition().getY(), 10);
+                //BOOST_CHECK_EQUAL(Crook.getPosition().getX(), 10);
+                BOOST_CHECK_EQUAL(Crook.getTypeID(), CROOK);
+                BOOST_CHECK_EQUAL(Crook.getName(), "crook");
+                BOOST_CHECK_EQUAL(Crook.getMovement(), 5);
+                BOOST_CHECK_EQUAL(Crook.getHealth(),100);
+
+                //BOOST_CHECK_EQUAL(Crook.isMapCell(), false);
+
+                Crook.setStatus(CharacterStatusID::SELECTED);
+                BOOST_CHECK_NE(Crook.getStatus(), CharacterStatusID::AVAILABLE);
+
+                Crook.setHealth(10,10);
+                BOOST_CHECK_EQUAL(Crook.getHealth(),3*10+2*10) ;
+
+                Crook.setMovementBonus(13,13);
+                BOOST_CHECK_EQUAL(Crook.getMovement(),7) ;
+
+                Crook.setDodge(15,15);
+                BOOST_CHECK_EQUAL(Crook.getDodge(),30/60);
+                
+                Crook.setPrecision(15,15,15,15);
+                BOOST_CHECK_EQUAL(Crook.getPrecision(),1);
+
+                
+                //Position p{10, 10};
+                //Crook.setPosition(p);
+                //BOOST_CHECK_EQUAL(Crook.getPosition().equals(p), true);
+                
+                
+                Crook.setName("newCrook");Crook.setMovement(5);
+                BOOST_CHECK_EQUAL(Crook.getName(),"newCrook");BOOST_CHECK_EQUAL(Crook.getMovement(),5);
+                
+                Character knigth(KNIGHT, "crook", 10, 10, 1);
+                BOOST_CHECK_EQUAL(knigth.getHealth(),80);
+                //Crook.setEffect(false,true,false);
+                //BOOST_CHECK_EQUAL(Crook.getEffect().getStunned(),true);
+
+                //Crook.setStats(14,14,14,14,14,14);
+                //BOOST_CHECK_EQUAL(Crook.getStats().getIntelligence(),14);
+
+
+         
+                //Position p2{-12, -32};
+                //BOOST_CHECK_GT(p.distance(p2), 0); // distance returns a positive int.
+
+                //Character c2{DISTANCE, true, "Shaker", 10, 10, 1};
+                //BOOST_CHECK_EQUAL(c.getPosition().equals(c2.getPosition()), true);
+
+                // inherited equal method from Element
+                //Character c1{STRENGHT, true, "Testy", 0, 0, 1};
+                //Character c1identical{STRENGHT, true, "Testy", 0, 0, 1};
+                //BOOST_CHECK_EQUAL(c1.equals(c1identical), true);
+
+
+
+
+            }
+
+
+        }
+}
