336dbf22fe38cb0f964f54593a1320c9bc735059
iceliades <theau.descatoire@ensea.fr>
2020-11-11 18:13:33 +0100

    modification to include test on Stats.cpp

--- test/shared/test_shared_state.cpp
+++ test/shared/test_shared_state.cpp
@@ -4,7 +4,7 @@
     #include <boost/test/unit_test.hpp>
 #include <string>
 #include "../../src/shared/state.h"
-#include "../../src/shared/state/Element.h"
+//#include "../../src/shared/state/Element.h"
 
 
 namespace state{
@@ -12,7 +12,7 @@ namespace state{
         BOOST_AUTO_TEST_CASE(TestStaticAssert)
         {
             BOOST_CHECK(1);
-        };
+        }
 
         BOOST_AUTO_TEST_CASE(TestState)
         {
@@ -23,5 +23,38 @@ namespace state{
             e.setName("Name");
             BOOST_CHECK_EQUAL(e.getTileCode(), 1);
             BOOST_CHECK_EQUAL(e.getName(), "Name");
-        };
-};
+//------------------------------------------------------ Stats ---------------------------------------------------------
+// Does the basic constructor do its job ?
+            Stats stats;
+            BOOST_CHECK_EQUAL(stats.getAgility(),8);
+            BOOST_CHECK_EQUAL(stats.getArcane(),8);
+            BOOST_CHECK_EQUAL(stats.getCourage(),8);
+            BOOST_CHECK_EQUAL(stats.getStamina(),8);
+            BOOST_CHECK_EQUAL(stats.getStrength(),8);
+
+//Does the 2nd constructor do its job ?
+            Stats stats2(0,0,0,0,0,0);
+            BOOST_CHECK_EQUAL(stats2.getAgility(),0);
+            BOOST_CHECK_EQUAL(stats2.getArcane(),0);
+            BOOST_CHECK_EQUAL(stats2.getCourage(),0);
+            BOOST_CHECK_EQUAL(stats2.getStamina(),0);
+            BOOST_CHECK_EQUAL(stats2.getStrength(),0);
+
+//Do the getters do their job ?
+            int fiveteen = 15;
+            stats2.setAgility(fiveteen);
+            stats2.setArcane(fiveteen);
+            stats2.setCourage(fiveteen);
+            stats2.setIntelligence(fiveteen);
+            stats2.setStamina(fiveteen);
+            stats2.setStrength(fiveteen);
+
+            BOOST_CHECK_EQUAL(stats2.getAgility(),15);
+            BOOST_CHECK_EQUAL(stats2.getArcane(),15);
+            BOOST_CHECK_EQUAL(stats2.getCourage(),15);
+            BOOST_CHECK_EQUAL(stats2.getStamina(),15);
+            BOOST_CHECK_EQUAL(stats2.getStrength(),15);
+
+
+        }
+}
