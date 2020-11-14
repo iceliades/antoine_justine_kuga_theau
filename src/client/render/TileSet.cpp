ac8acdc3013b28a267251b308869ec9a3b2e1442
Kuga23 <ku.ga@hotmail.fr>
2020-11-14 17:06:34 +0100

    state and render modif

---
+++ src/client/render/TileSet.cpp
@@ -0,0 +1,38 @@
+#include <string>
+/*#include "../render.h"
+
+using namespace render;
+
+// Constructor
+
+TileSet::TileSet(TileSetID newID){
+    id = newID;
+    
+    switch(id){
+        case MAPTILESET:
+            cellWidth=32;
+            cellHeight=32;
+            //imageFile="rsc/Images/characters_tileset_32.png";
+            break;
+        case CHARTILESET:
+            cellWidth=32;
+            cellHeight=32;
+            imageFile="rsc/Images/cursor_32.png";
+            break;
+        default:
+            break;
+    }
+
+    //textureTileSet.loadFromFile(imageFile);
+}
+
+// Getters
+
+int const TileSet::getCellWidth(){
+    return cellWidth;
+}
+
+int const TileSet::getCellHeight(){
+    return cellHeight;
+}*/
+
