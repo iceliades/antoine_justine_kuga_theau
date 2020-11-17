8f5fdada40e6e2d55c1a0823a13c6692c06d4a62
iceliades <theau.descatoire@ensea.fr>
2020-11-17 16:25:17 +0100

    small amount of modifications

--- src/shared/state/Character.cpp
+++ src/shared/state/Character.cpp
@@ -1,4 +1,5 @@
 #include "Character.h"
+#include "Player.h"
 #include "State.h"
 #include <fstream>
 #include <iostream>
@@ -13,47 +14,71 @@ using namespace state;
 Character::Character(CharacterTypeID id, std::string name, int x, int y, int char_tileCode) {
 
     this->tileCode = char_tileCode;
+    position.setX(x);
+    position.setY(y);
+
     TypeID = id;
     Name = name;
     Status = AVAILABLE;
-    position.setX(x);
-    position.setY(y);
-    //charWeap= new Weapon(SWORD);
+    charWeap= new Weapon(SWORD);
+    effect = Effect();
     Movement = 5;
+    MovementLeft= 5;
+    PlayerID = 0;//Player->getListCharacters().size;
+    direction = DOWN;
 
     switch (id) {
-        case ( CROOK) :
-        Health = 100;
-        Precision = 2;
-        Dodge = 0;
-        // stats.setAgility(stats.getAgility()+1);// Taxon bonus
+        case (CROOK) :
+            Health = 100;
+            Precision = 2.0;
+            Dodge = 0.0;
+            //  stats.setIntelligence(stats.getIntelligence()+1);
         break;
-        case ( KNIGHT) :
-        Health = 80;
-        Precision = 3;
-        Dodge = 0;
-        //  stats.setAgility(stats.getAgility()+1);
+        case (KNIGHT) :
+            Health = 80;
+            Precision = 3.0;
+            Dodge = 0.0;
+            //  stats.setCourage(stats.getCourage()+1);
         break;
-        case ( ELF) :
+        case (ELF) :
+            Health = 80;
+            Precision = 3.0;
+            Dodge = 0.0;
+            //  stats.setAgility(stats.getAgility()+1);
         break;
-        case ( NATIVE) :
+        case (NATIVE) :
+            Health = 80;
+            Precision = 3.0;
+            Dodge = 0.0;
+            //  stats.setArcane(stats.getArcane()+1);
         break;
-        case ( DWARF) :
+        case (DWARF) :
+            Health = 80;
+            Precision = 3.0;
+            Dodge = 0.0;
+            //  stats.setStamina(stats.getStamina()+1);
         break;
-        case ( PIRATE) :
+        case (PIRATE) :
+            Health = 80;
+            Precision = 3.0;
+            Dodge = 0.0;
+            //  ANNOYING WE NEED A WAY TO GET THE PLAYER'S CHOICE
         break;
-        case ( TROLL) :
+        case (TROLL) :
+            Health = 80;
+            Precision = 3.0;
+            Dodge = 0.0;
+            //  stats.setStrength(stats.getStrength()+1);
         break;
 
 }
 }
 Character::~Character(){
-    //delete charWeap;
+    delete charWeap;
 }
 
 //------------------------------------------------- Setters ------------------------------------------------------------
 void Character::setHealth ( int Stamina,  int Strength){
-
     Health= 3*Stamina + 2*Strength;
 }
 
@@ -68,11 +93,12 @@ void Character::setDodge (int Agility, int Intelligence){
 void Character::setPrecision ( int Agility, int Intelligence, int Strength, int Arcane){
     Precision= (Agility+Intelligence+Strength+Arcane)/60;
 }
-/*void Character::setEffect (bool Immobilised,bool Stunned,bool Disarmed){
+
+void Character::setEffect (bool Immobilised,bool Stunned,bool Disarmed){
     effect.setImmobilised(Immobilised);
     effect.setStunned(Stunned);
     effect.setDisarmed(Disarmed);
-};
+}
 
 void Character::setCharWeap (Weapon* w){
     charWeap=w;
@@ -86,10 +112,11 @@ void Character::setStats (int Agility, int Arcane, int Courage, int Intelligence
     stats.setStamina(Stamina);
     stats.setStrength(Strength);
 }
-*/
+
 
 void Character::setTypeID(CharacterTypeID TypeID){
     // TypeID can't change
+    cout << "What are you doing ?!\n";
 }
 void Character::setName(const std::string& Name){
     this->Name=Name;
@@ -102,19 +129,39 @@ void Character::setMovement(int Movement){
 }
 
 //------------------------------------------------- Getters ------------------------------------------------------------
-CharacterTypeID Character::getTypeID() {return TypeID;}
-std::string   Character::getName (){return Name;}
-CharacterStatusID Character::getStatus (){return Status;};
-int Character::getMovement (){return Movement;}
-int Character::getHealth (){return Health;}
-float Character::getDodge (){return Dodge;}
-float Character::getPrecision (){return Precision;}
+CharacterTypeID Character::getTypeID() {
+    return TypeID;
+}
+
+std::string   Character::getName (){
+    return Name;
+}
+
+CharacterStatusID Character::getStatus (){
+    return Status;
+}
+
+int Character::getMovement (){
+    return Movement;
+}
+
+int Character::getHealth (){
+    return Health;
+}
+
+float Character::getDodge (){
+    return Dodge;
+}
+
+float Character::getPrecision (){
+    return Precision;
+}
 
 Effect& Character::getEffect (){
-    return refEff;
+    return effect;
 }
 Stats& Character::getStats (){
-    return refStats;
+    return stats;
 }
 Weapon* Character::getCharWeap (){
     return charWeap;
