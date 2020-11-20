7367e9a824a723937ba5e337fca1bef4bf505840
Kuga23 <ku.ga@hotmail.fr>
2020-11-20 15:44:22 +0100

    delete last commit Antoine

--- src/test/client/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vincent/Bureau/PLT/antoine_justine_kuga_theau/test/client
-# Build directory: /home/vincent/Bureau/PLT/antoine_justine_kuga_theau/src/test/client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_client_dummy "/home/vincent/Bureau/PLT/antoine_justine_kuga_theau/src/test/client/test_client_dummy")
