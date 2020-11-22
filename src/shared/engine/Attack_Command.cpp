bbff7fcd52ba07ea3e17c64f4175ac008e9e5de5
Kuga23 <ku.ga@hotmail.fr>
2020-11-22 17:19:56 +0100

    debug minor errors

--- src/shared/engine/Attack_Command.cpp
+++ src/shared/engine/Attack_Command.cpp
@@ -18,29 +18,22 @@ void Attack_Command::exec(state::State& curState)
 {
 	cout << attacker.getName() << "is trying to attack" << target.getName() << endl;
 	cout << "Actual health of " << target.getName() << ": " << target.getHealth() << endl;
-	
-	default_random_engine generator
-	bernoulli_distribution precision(attacker.getPrecision());
-	bernoulli_distribution dodge(1-target.getDodge());
-    
-
+	    
 	for(auto& index: attacker.allowedAttackTarget(curState)){
 		if( index==target.getIndex()){
 			int chardmg= attacker.getCharWeap()->getDammages();
-			// need to produce the final using dodge and precision
-			if (precision(generator) && dodge(generator)){
-				nt newtarHealth= target.getHealth()- chardmg;
-				target.setNewHealth(newtarHealth);
-				
-				if (target.getHealth()<= 0){
-					target.setStatus(DEATH);
-					cout<<"U KILLED THE CHARACTER"<<target.getName()<<endl;
-				}
-				
-				cout << "THE TARGET HEALTH"<< target.getHealth()<<endl;
-				cout<<"\n";
+			// need to produce the final using dodge and precision	
+			int newtarHealth= target.getHealth()- chardmg;
+			target.setNewHealth(newtarHealth);
+			
+			if (target.getHealth()<= 0){
+				target.setStatus(DEATH);
+				cout<<"U KILLED THE CHARACTER"<<target.getName()<<endl;
 			}
-			else cout << "Sorry, you missed... Retry next turn or die before..." << endl;
+			
+			cout << "THE TARGET HEALTH"<< target.getHealth()<<endl;
+			cout<<"\n";
+					
 		}
 	}	
 
--- src/shared/state/Character.cpp
+++ src/shared/state/Character.cpp
@@ -84,7 +84,7 @@ void Character::setCharWeap (Weapon* w){
 }
 
 void Character::setDodge (int Agility, int Intelligence){
-    Dodge= 0,33 * ((Agility+Intelligence-16)**2)/196;
+    Dodge= 0,33 * ((Agility+Intelligence-16)^2)/196;
 }
 
 void Character::setEffect (bool Immobilised,bool Stunned,bool Disarmed){
@@ -195,20 +195,10 @@ int Character::getPlayerID(){
     return this->PlayerID;
 }
 
-vector<int>& getCapab()
-{
-	return Capab;
-}
 //----------------------------------------------------- Misc -----------------------------------------------------------
 bool Character::isMapCell(){
     return false;
 }
-
-void addCapab(int compt)
-{
-	Capab.push_back(move(compt));
-}
-
 
 std::vector<Position> Character::allowedMove(State& state){
     
