b1223cb01a5aeb0fe72be295bdb0b03eb70cef87
justineCharlet <justine.charlet@hotmail.fr>
2020-12-09 09:59:35 +0100

    fonction

--- src/shared/ai/HeuristicAI.cpp
+++ src/shared/ai/HeuristicAI.cpp
@@ -1,4 +1,4 @@
-//#include "HeuristicAI.h"
+#include "HeuristicAI.h"
 #include "engine.h"
 #include <iostream>
 #include <stdlib.h>
@@ -6,12 +6,23 @@
 
 
 
-//using namespace ai;
+using namespace ai;
 using namespace std;
 using namespace state;
 using namespace engine;
 
 
+HeuristicAI::AI(int nbplayers){
+    this->nbplayers = nbplayers;
+}
+
+
+
+
+void HeuristicAI::run(engine::Engine& engine){
+
+
+}
 
 
 
