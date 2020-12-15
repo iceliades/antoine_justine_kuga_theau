c3bddf4e7012481a72b33a8d5c10413daa8a1de5
Kuga23 <ku.ga@hotmail.fr>
2020-12-15 19:10:33 +0100

    hai_vs_rai

--- src/client/client/Command_Client_RHAI.cpp
+++ src/client/client/Command_Client_RHAI.cpp
@@ -25,18 +25,20 @@ Command_Client_RHAI::~Command_Client_RHAI(){
 }
 
 void Command_Client_RHAI::execute() {
-    std::vector<int> stats;
+    srand(time(NULL));
+    std::vector<int> stats(2,0);
+    int nbEpisode=50; 
     // Random_ai vs Random_ai
-    for (int i = 0; i<100; i++)
+    for (int i = 0; i<nbEpisode; i++)
     {
-        srand(time(NULL));// Init random generator
-
+       // Init random generator
         Engine myEngine; // No confusion to engine packgage
         myEngine.getState().setMode("random_ai");
         myEngine.getState().initPlayers();
         myEngine.getState().initCharacters();
         myEngine.getState().initMapCell();
 
+        std::cout.setstate(std::ios_base::failbit);
         // Init ai
         ai::HeuristicAI heuristicAI(myEngine,1);
         ai::RandomAI randomAi;
@@ -48,15 +50,16 @@ void Command_Client_RHAI::execute() {
             if(myEngine.getState().getCurPlayerID()==1) heuristicAI.run(myEngine);
             else randomAi.run(myEngine);
         }
-
+    
         if(myEngine.getState().getEndGame()==true)
         {
             if (myEngine.getState().getGameWinner()==1) stats[0] += 1;
             else stats[1] += 1;
         }
-
-        cout << "Heuristic AI victories : " << stats[0] << endl;
-        cout << "Random AI victories : " << stats[1] << endl;
-        cout << "Percentage win of Heuristic AI : " << stats[0]/(stats[0]+stats[1]) << endl;
+        std::cout.clear();
+        std::cout << "Heuristic AI victories : " << stats[0] << endl;
+        std::cout << "Random AI victories : " << stats[1] << endl;
+        //myEngine.~Engine();
     }
+    std::cout << "Percentage win of Heuristic AI : " << stats[0]*100/(stats[0]+stats[1]) << endl;
 }
