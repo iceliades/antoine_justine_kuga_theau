c08ef5defe6b50516df763f90e1048a3234bc616
justineCharlet <justine.charlet@hotmail.fr>
2020-12-24 13:20:03 +0100

    début DeepAI

---
+++ src/shared/ai/DeepAI.cpp
@@ -0,0 +1,22 @@
+#include "DeepAI.h"
+
+using namespace std;
+using namespace ai;
+
+DeepAI::DeepAI(engine::Engine& myEngine, int nbPlayers){
+
+}
+
+
+DeepAI::~DeepAI(){
+    delete currState;
+}
+
+
+
+void DeepAI:run(engine::Engine& myEngine){
+
+    
+}
+
+
