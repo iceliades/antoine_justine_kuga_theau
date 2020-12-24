901a51635239a841de1acb902c78a0090e780953
justineCharlet <justine.charlet@hotmail.fr>
2020-12-24 12:41:28 +0100

    test error

---
+++ test/client/test_client_client.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Justine on 17/12/2020.
+// 
+#include <boost/test/unit_test.hpp>
+#include "../../src/client/client.h"
+#include "../../src/shared/state.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+
+namespace client{
+
+    BOOST_AUTO_TEST_CASE(TestStaticAssert)
+        {
+            BOOST_CHECK(1);
+        }
+    
+    BOOST_AUTO_TEST_CASE(TestClient)
+        {   
+//--------------------------------------------------- Caretaker -------------------------------------------------------
+            {
+                // Caretaker
+                
+                Caretaker care();
+                std::vector<MemoryState> base = care->&getSavedMemories();
+
+                BOOST_TEST(base == vectNull);
+
+
+                
+            }
+            
+ 
+        
+
+
+
+        }
+
+}
