488e50e7638b799d1a3e6ef295c868b1d663a8a1
AntoinespacePierre <amontmaur@gmail.com>
2021-01-07 11:51:57 +0100

    minimax changes

--- src/shared/ai/Node.cpp
+++ src/shared/ai/Node.cpp
@@ -10,8 +10,8 @@ using namespace ai;
 
 Node::Node(state::State& currState) : char_p1(currState.getListCharacters(0)), char_p2(currState.getListCharacters(1))
 {
-    for (int i = 0; i < char_p1.size(); i++) value += char_p1[i]->getHealth();
-    for (int i = 0; i < char_p2.size(); i++) value -= char_p2[i]->getHealth();
+    for (size_t i = 0; i < char_p1.size(); i++) value += char_p1[i]->getHealth();
+    for (size_t i = 0; i < char_p2.size(); i++) value -= char_p2[i]->getHealth();
 } 
 
 
