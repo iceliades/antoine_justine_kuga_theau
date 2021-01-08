76f9fb935aab59f90f5248565966fb6f042adec6
iceliades <iceliades98@gmail.com>
2021-01-08 20:39:06 +0100

    now lists all commands when there is an error

--- src/client/client/Command_Client_Autre.cpp
+++ src/client/client/Command_Client_Autre.cpp
@@ -10,6 +10,7 @@ Command_Client_Autre::Command_Client_Autre() {
 Command_Client_Autre::~Command_Client_Autre(){
 }
 void Command_Client_Autre::execute(){
-    cout <<" Cette commande est inexistante"<<endl;
+    cout <<"Cette commande est inexistante \nVeuillez utilisez un des paramétres suivants :\nhello\nrender\nrandom_ai\nplayer_vs_rai\nengine"<<
+    "\nheuristic_ai\nplayer_vs_hai\nhai_vs_rai\nrollback\nplay\nthread_hai\n"<<endl;
     //cout << "Veillez utiliser les commandes make unittest et make code-coverage depuis le dossier build" << endl;
 }
