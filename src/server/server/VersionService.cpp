1bee018c120b456f26358fe8dc9b1c08767d8713
justineCharlet <justine.charlet@hotmail.fr>
2021-01-18 19:37:28 +0100

    réglement des erreurs

--- src/server/server/VersionService.cpp
+++ src/server/server/VersionService.cpp
@@ -4,8 +4,7 @@ using namespace std;
 using namespace server;
 
 
-VersionService::VersionService(){
-    this->AbstractService("/version");
+VersionService::VersionService() : AbstractService("/version"){
 }
 
 HttpStatus const VersionService::get(Json::Value& out, int in){
